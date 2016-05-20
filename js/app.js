7d1e5da3a45507a435ddd889dd600ceff159d862
David Sanchez <d.czanches@gmail.com>
2016-05-19 17:29:09 -0700

    Added the use of fetch to call Fibti API

--- js/app.js
+++ js/app.js
@@ -1,10 +1,36 @@
 // Main application
 var url = window.location.toString();
 var access_token;
+var today = new Date();
 
-if(url.indexOf('#') == -1) {
+if(url.indexOf('#') === -1) {
     // Send user through auth
     auth(app);
 } else {
     access_token = pullToken(url);
+
+    fetch('https://api.fitbit.com/1/user/-/activities/date/' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '.json', {
+        method: 'GET',
+        headers: new Headers({
+            "Authorization": "Bearer " + access_token
+        }),
+        mode: 'cors'
+    }).then(function(response) { // Processing response
+        if(!response.ok) {
+            throw new Error('Fitbit API request failed: ' + response);
+            window.alert('Fitbit API request failed: ' + response);
+        }
+
+        var type = response.headers.get('Content-Type');
+
+        if(type && type.indexOf('application/json') !== -1) {
+            return response.json();
+        } else {
+            throw new Error('Didn\'t receive JSON... ' + response);
+            window.alert('Didn\'t receive JSON... ' + response);
+        }
+    }).then(function(json) {
+        console.log('JSON received...' + json);
+        window.alert('Your steps today: ' + json['summary'].steps);
+    });
 }
