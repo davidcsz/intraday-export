c6fe09e9f853319e3dccbc2277063c4e2dd331a9
David Sanchez <d.czanches@gmail.com>
2016-05-21 01:09:45 -0700

    Updated redirect_uri to point to Raspberry Pi

--- config/app-config.js
+++ config/app-config.js
@@ -3,6 +3,6 @@ var app = {
     client_id : '227DW9',
     response_type : 'token',
     scope : 'activity heartrate location nutrition profile settings sleep social weight',
-    redirect_uri : 'http://84561499.ngrok.io', // Change based on ngrok
+    redirect_uri : 'http://192.168.0.222:8080', // Change based on ngrok
     prompt : 'login'
 };
