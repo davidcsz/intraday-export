e20b456aa645583055216fed9ea0c03036a7d13c
dczanches <d.czanches@gmail.com>
2016-09-27 19:04:56 -0700

    Reboot.

--- config/app-config.js
+++
@@ -1,8 +0,0 @@
-var app = {
-    auth_host : 'https://www.fitbit.com/oauth2/authorize',
-    client_id : '227DW9',
-    response_type : 'token',
-    scope : 'activity heartrate location nutrition profile settings sleep social weight',
-    redirect_uri : 'http://192.168.0.222:8080', // Change based on ngrok
-    prompt : 'login'
-};
